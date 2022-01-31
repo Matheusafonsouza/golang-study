79daf1d88b2f8c01c74e9a2edf057d4227cb5653
matheusafonsouza <matheusafonsouza@gmail.com>
2022-01-31 00:46:54 -0300

    create a get functino for prefix

--- hello/main.go
+++ hello/main.go
@@ -6,12 +6,7 @@ const HELLO_ENGLISH = "Hello, "
 const HELLO_SPANISH = "Hola, "
 const HELLO_FRENCH = "Bonjour, "
 
-func hello(name string, language string) string {
-	if name == "" {
-		name = "world"
-	}
-	prefix := HELLO_ENGLISH
-
+func getPrefix(language string) (prefix string) {
 	switch language {
 	case "english":
 		prefix = HELLO_ENGLISH
@@ -19,7 +14,17 @@ func hello(name string, language string) string {
 		prefix = HELLO_FRENCH
 	case "spanish":
 		prefix = HELLO_SPANISH
+	default:
+		prefix = HELLO_ENGLISH
 	}
+	return
+}
+
+func hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	prefix := getPrefix(language)
 	return prefix + name
 }
 
