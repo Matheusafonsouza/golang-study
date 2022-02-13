59a4525709a9227e9ef9e31f2dd2d479cb2e48d7
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-13 00:31:31 -0300

    remove main

--- slices/main.go
+++ slices/main.go
@@ -1,16 +1,9 @@
 package main
 
-import "fmt"
-
 func sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
-}
-
-func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-	fmt.Println(sum(numbers))
 }
