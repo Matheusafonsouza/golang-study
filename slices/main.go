dfbf0a969b44b98be301241928a145194e9151eb
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-13 00:36:57 -0300

    sum all slices function

--- slices/main.go
+++ slices/main.go
@@ -7,3 +7,13 @@ func sum(numbers []int) int {
 	}
 	return sum
 }
+
+func sumAll(numberSlices ...[]int) (sums []int) {
+	sums = make([]int, len(numberSlices))
+
+	for i, numbers := range numberSlices {
+		sums[i] = sum(numbers)
+	}
+
+	return
+}
--- slices/main_test.go
+++ slices/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	verifyMessage := func(t *testing.T, result int, expected int) {
@@ -14,6 +17,21 @@ func TestSum(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		result := sum(numbers)
 		expected := 6
+		verifyMessage(t, result, expected)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	verifyMessage := func(t *testing.T, result []int, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Result %d, expected %d", result, expected)
+		}
+	}
+
+	t.Run("Should sum all slices in one slice", func(t *testing.T) {
+		result := sumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{3, 9}
 		verifyMessage(t, result, expected)
 	})
 }
